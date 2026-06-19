313a5e626049e738a32dca8b99729df59d9705fa
vitor.soares <vitor.soares@phoebustecnologia.com.br>
2026-06-19 14:47:33 -0300

    chore: refatora estrutura

--- src/config/db.js
+++ src/config/db.js
@@ -1,25 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.set('bufferCommands', false);
+mongoose.set("bufferCommands", false);
 
 let isConnected = null;
 
-const connectDataBase = async() => {
-    if(mongoose.connection.readyState === 1 || mongoose.connection.readyState === 2) {
-        console.log('=> Usando conexão existente com o MongoDB');
-        return;
-    };
+const connectDataBase = async () => {
+  if (
+    mongoose.connection.readyState === 1 ||
+    mongoose.connection.readyState === 2
+  ) {
+    console.log("=> Usando conexão existente com o MongoDB");
+    return;
+  }
 
-    console.log('=> Criando NOVA conexão com o MongoDB');
+  console.log("=> Criando NOVA conexão com o MongoDB");
 
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Conectado ao MongoDB com sucesso!")
-    } catch (error) {
-        console.error("Erro ao conectar no MongoDB: ", error.message);
-        throw error;
-    }
-
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log("Conectado ao MongoDB com sucesso!");
+  } catch (error) {
+    console.error("Erro ao conectar no MongoDB: ", error.message);
+    throw error;
+  }
 };
 
-module.exports = {connectDataBase};
+module.exports = { connectDataBase };
